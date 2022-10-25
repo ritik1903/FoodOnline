e9f6a69c2019fd1b0b1d97d1209a557f9c3f3a6d
ritik1903 <71216958+ritik1903@users.noreply.github.com>
2022-10-25 21:08:38 +0530

    Vendor Registration

--- accounts/models.py
+++ accounts/models.py
@@ -41,11 +41,12 @@ class UserManager(BaseUserManager):
         return user
 
 class User(AbstractBaseUser):
-    RESTAURANT=1
+    VENDOR=1
     CUSTOMER=2
+    
     ROLE_CHOICE = (
-        (RESTAURANT, 'Restaurant'),
-        (CUSTOMER, 'CUSTOMER'),
+        (VENDOR, 'Vendor'),
+        (CUSTOMER, 'Customer'),
     )
     
     first_name = models.CharField(max_length=50)
