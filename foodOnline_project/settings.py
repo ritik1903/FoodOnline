d7a17087d6feca48d5e5e227e45b7e33ea7d23ba
ritik1903 <71216958+ritik1903@users.noreply.github.com>
2022-11-02 13:51:19 +0530

    Forgot Password Feature

--- foodOnline_project/settings.py
+++ foodOnline_project/settings.py
@@ -143,3 +143,12 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+
+# Email Configuration
+from decouple import config
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'ZoneoFood Marketplace <subhampawar41599@gmail.com>'
