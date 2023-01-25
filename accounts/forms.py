d155d611c5cf8753abfbdd3c627fb0bfb92817fc
ritik1903 <71216958+ritik1903@users.noreply.github.com>
2023-01-25 23:30:06 +0530

    Vendor profile form validation

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from .models import User
+from .models import User, UserProfile
+from .validators import allow_only_images_validator
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
@@ -18,3 +19,20 @@ class UserForm(forms.ModelForm):
                 "Password does not match!"
             )
         
+class UserProfileForm(forms.ModelForm):
+    address = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'start typing...', 'required': 'required'}))
+    profile_picture = forms.FileField(widget=forms.FileInput(attrs={'class' : 'btn btn-info'}), validators=[allow_only_images_validator])
+    cover_photo = forms.FileField(widget=forms.FileInput(attrs={'class' : 'btn btn-info'}), validators = [allow_only_images_validator])
+    
+    latitude = forms.CharField(widget=forms.TextInput(attrs={'readonly' : 'readonly'}))
+    longitude = forms.CharField(widget=forms.TextInput(attrs={'readonly' : 'readonly'}))
+    class Meta:
+        model = UserProfile
+        fields = ['profile_picture', 'cover_photo', 'address', 'country', 'state', 'city', 'pin_code', 'latitude', 'longitude']
+
+    #Another way of do readonly longitude and latitude 
+    # def __init__(self, *args, **kwargs):
+    #     super(UserProfileForm, self).__init__(*args, **kwargs)
+    #     for field in self.fields:
+    #         if field == 'latitude' or field == 'longitude':
+    #             self.fields[field].widget.attrs['readonly'] = 'readonly'
