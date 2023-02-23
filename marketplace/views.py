8d8eb4a8ffdfb1637862f3c97535767542d99db6
ritik1903 <71216958+ritik1903@users.noreply.github.com>
2023-02-23 18:05:28 +0530

    Search functionality

--- marketplace/views.py
+++ marketplace/views.py
@@ -117,3 +117,13 @@ def delete_cart(request,cart_id):
                 return JsonResponse({'status':'Failed','message':'Cart item does not exist!'})
         else:
              return JsonResponse({'status':'Failed','message':'Invalid request!'})
+
+def search(request):
+    address = request.GET['address']
+    latitude = request.GET['lat']
+    longitude = request.GET['lng']
+    radius = request.GET['radius']
+    r_name = request.GET['rest_name']
+    
+    
+    return render(request, 'marketplace/listing.html')
